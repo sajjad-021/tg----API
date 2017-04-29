49e34ec6327c3c041c5757eeafdc07764c3b1797
sajjad_021 <sajjad-021@users.noreply.github.com>
2017-04-29 12:14:27 -0700

    tgGuard v7

---
+++ libs/tg-cli.lua
@@ -0,0 +1,11 @@
+
+default_profile = "default";
+
+default = {
+  config_directory = "./tgAPI/data/telegram-cli";
+  auth_file = "./tgAPI/telegram-cli/auth";
+  test = false;
+  msg_num = true;
+  log_level = 2;
+};
+
